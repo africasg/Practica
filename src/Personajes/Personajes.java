1fac3ad8e51c66aab66f1699dabc9f6a7ae9fe30
alejandraserranoperez <jandrax2005@gmail.com>
2024-12-18 20:20:07 +0100

    JUEGO HECHO

--- src/Personajes/Personajes.java
+++ src/Personajes/Personajes.java
@@ -6,24 +6,21 @@ public class Personajes {
     private String nombre;
     private ImageIcon imagen;
     private String descripcion;
+
     //posicion imagen (en el main del juego)
     public Personajes(String nombre, String ruta, String descripcion) {
         this.nombre = nombre;
         this.imagen = new ImageIcon(ruta);
         this.descripcion = descripcion;
     }
+
     /**
      * metodos: aparecer
-
      */
     public String getNombre() {
         return nombre;
     }
 
-    public String getDescripcion() {
-        return descripcion;
-    }
 }
-
 
 
